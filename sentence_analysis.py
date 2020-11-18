ea374f7b272a61cdfc440b454aa25de87c2e8e06
alisoltanirad <soltaniradali@gmail.com>
2020-11-18 13:51:03 +0330

    Test grammar function

--- sentence_analysis.py
+++ sentence_analysis.py
@@ -3,7 +3,11 @@
 import nltk
 
 def main():
-    pass
+    grammar = nltk.data.load('file:grammar.cfg')
+    sentence = 'Mary saw Bob'.split()
+    rd_parser = nltk.RecursiveDescentParser(grammar)
+    for tree in rd_parser.parse(sentence):
+        print(tree)
 
 
 if __name__ == '__main__':
